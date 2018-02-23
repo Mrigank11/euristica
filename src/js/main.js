feb9e9f33093f0e38ec35c44f8820cd1c3a3c3d7
Mrigank Krishan <mrigankkrishan@gmail.com>
2018-02-24 00:18:35 +0530

    Minor fixes
    add "autohide nav" feature
    animation tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
-import Vue from "vue/dist/vue.js";
+import Vue from "vue/dist/vue.min.js";
 import { TweenMax, Power4, Back } from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
-import data from './data';
+import sitedata from './sitedata';
 
 const $menu = ".menu";
 const $burger = document.getElementsByClassName("navbar-burger")[0];
 const $nav = document.getElementsByTagName("nav")[0];
 const $navbarItems = ".list>.navbar-item";
 
+//region init Vue
 const app = new Vue({
     el: "#main",
     data: {
@@ -16,7 +17,7 @@ const app = new Vue({
             name: "",
             email: ""
         },
-        sitedata: data,
+        sitedata: sitedata,
         se: null
     },
     methods: {
@@ -26,13 +27,13 @@ const app = new Vue({
         },
         toggleMenu() {
             TweenMax.set($navbarItems, { scale: 0 });
-            TweenMax.to($menu, 0.5, { opacity: !this.menuVisible });
+            TweenMax.to($menu, 0.2, { opacity: !this.menuVisible });
 
             if (this.menuVisible) {
                 TweenMax.set($menu, { visibility: 'hidden', delay: 0.5 });
             } else {
                 TweenMax.set($menu, { visibility: 'visible' });
-                TweenMax.staggerTo($navbarItems, 0.5, { scale: 1, ease: Back.easeOut }, 0.2);
+                TweenMax.staggerTo($navbarItems, 0.2, { scale: 1, ease: Back.easeOut, delay: 0.1 }, 0.1);
             }
             this.menuVisible = !this.menuVisible;
         },
@@ -41,13 +42,13 @@ const app = new Vue({
         },
         showEvent(event) {
             this.$set(this, "se", event);
-            Vue.nextTick(function () {
+            Vue.nextTick(() => {
                 TweenMax.fromTo("#event-modal-card", 0.5, { scaleY: 0 }, { scaleY: 1, ease: Back.easeOut });
             });
         },
         dismissEvent() {
             TweenMax.fromTo("#event-modal-card", 0.2, { scaleY: 1 }, {
-                scaleY: 0, ease: Power4.easeOut, onComplete: function () {
+                scaleY: 0, ease: Power4.easeOut, onComplete: () => {
                     this.$set(this, "se", null);
                 },
                 onCompleteScope: this
@@ -55,3 +56,19 @@ const app = new Vue({
         }
     }
 });
+//endregion
+
+//region auto hide nav
+var $lastScroll = -1;
+window.addEventListener("scroll", () => {
+    var scroll = window.scrollY;
+    if (scroll > $lastScroll) {
+        if (app.menuVisible) return;
+        //hide 
+        TweenMax.to("nav", 0.7, { yPercent: -100, ease: Power4.easeOut });
+    } else {
+        TweenMax.to("nav", 0.7, { yPercent: 0, ease: Power4.easeOut });
+    }
+    $lastScroll = scroll;
+});
+//endregion
